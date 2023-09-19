760d555b41fcf2029604a52d9b880ab18757758b
amansgz <amansgz.ar@gmail.com>
2023-09-19 20:51:32 -0300

    Add styles desktop

--- scripts/app.js
+++ scripts/app.js
@@ -1,25 +1,25 @@
 const STUDENTS = [
   {
-      name: 'Tanya Sinclair',
-      title: 'UX Engineer',
-      img: './images/image-tanya.jpg',
-      alt: 'Tanya Sinclair photo',
-      testimonial: '“ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ”',
+    name: 'Tanya Sinclair',
+    title: 'UX Engineer',
+    img: './images/image-tanya.jpg',
+    alt: 'Tanya Sinclair',
+    testimonial: '“ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ”',
   },
   {
-      name: 'John Tarkpor',
-      title: 'Junior Front-end Developer',
-      img: './images/image-john.jpg',
-      alt: 'John Tarkpor photo',
-      testimonial: '“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”',
+    name: 'John Tarkpor',
+    title: 'Junior Front-end Developer',
+    img: './images/image-john.jpg',
+    alt: 'John Tarkpor',
+    testimonial: '“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”',
   }
 ];
 
 
-const nameStudent = document.getElementById('name-student');
-const title = document.getElementById('title-student');
 const photo = document.getElementById('student-photo');
 const testimonial = document.getElementById('testimonial');
+const nameStudent = document.getElementById('name-student');
+const title = document.getElementById('title-student');
 
 const btnPrev = document.getElementById('btn-prev');
 const btnNext = document.getElementById('btn-next');
